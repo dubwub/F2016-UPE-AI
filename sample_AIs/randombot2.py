9820ad649103c9649f08d37bd230b7171b613fbd
Darwin Ding <dingdarwin@gmail.com>
2016-10-18 20:45:02 -0400

    major UI overhauls

--- sample_AIs/randombot2.py
+++ sample_AIs/randombot2.py
@@ -5,7 +5,7 @@
 import time # for testing, this is not good
 import requests # if not installed already, run python -m pip install requests OR pip install requests, whatever you normally do
 import random
-r = requests.post('http://localhost:3000/api/games/search', data={'devkey': "57ebe693534fe7fc2841d5d6", 'username': 'darwinbot2'}) # search for new game
+r = requests.post('http://upe21.cs.rpi.edu:3000/api/games/search', data={'devkey': "580685212e7f4d4b0a89fc7c", 'username': 'darwinbot2'}) # search for new game
 json = r.json() # when request comes back, that means you've found a match! (validation if server goes down?)
 print(json)
 gameID = json['gameID']
@@ -16,7 +16,7 @@ possibleMoves = ['mu', 'ml', 'mr', 'md', 'tu', 'tl', 'tr', 'td', 'b', '', 'op', 
 output = {'state': 'in progress'}
 while output['state'] != 'complete':
 	randomInt = random.randint(0,len(possibleMoves)-1)
-	r = requests.post('http://localhost:3000/api/games/submit/' + gameID, data={'playerID': playerID, 'move': possibleMoves[randomInt], 'devkey': "57ebe693534fe7fc2841d5d6"}); # submit sample move
+	r = requests.post('http://upe21.cs.rpi.edu:3000/api/games/submit/' + gameID, data={'playerID': playerID, 'move': possibleMoves[randomInt], 'devkey': "580685212e7f4d4b0a89fc7c"}); # submit sample move
 	json = r.json()
 	print(json)
 	output = json
