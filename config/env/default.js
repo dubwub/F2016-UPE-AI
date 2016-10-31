9a36af14bc18d08256fe81dbc049021d83af3689
Darwin Ding <dingdarwin@gmail.com>
2016-10-31 18:26:47 -0400

    UI updates, prepping for launch

--- config/env/default.js
+++ config/env/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   app: {
-    title: 'AICOMP.io',
+    title: 'AICOMP.ME',
     description: 'RPI and UPEs first ever AI competition!',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
